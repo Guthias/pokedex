ff16251409d59ee712c33c1188ec15ff680497b7
unknown <gmgustavom@gmail.com>
2022-03-21 23:08:58 -0300

    feat: rendering Pokemon List

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as api from './services/API';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  const [pokemonList, updatePokemonList] = useState([]);
+
   useEffect(() => {
     const fetchPokemonList = async () => {
       const data = await api.getPokemonList();
-      console.log(data);
+      updatePokemonList(data);
     }
     fetchPokemonList();
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {
+          pokemonList.map(({ name }) => <li>{ name }</li>)
+        }
+      </ul>
     </div>
   );
 }
